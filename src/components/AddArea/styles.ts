e161f3cfe52f437eae2e30bac28c6a82f6cc57e4
Thiago <thiagooffice2@gmail.com>
2021-11-23 18:46:30 -0300

    Projeto completo

---
+++ src/components/AddArea/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    border: 1px solid #555;
+    border-radius: 15px;
+    padding: 10px;
+    margin: 20px 0;
+    display: flex;
+    align-items: center;
+
+    .image{
+        margin-right: 8px;
+        background: #fff;
+        border-radius: 25px;
+        padding: 5px;
+    }
+    input{
+        border: 0;
+        background: transparent;
+        outline: 0;
+        color: #fff;
+        font-size: 20px;
+        flex: 1;
+    }
+`
